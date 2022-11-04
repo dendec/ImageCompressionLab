26cbc2af2d06b67475a6d5410ad6c66fcfc94d66
Denis Dechev <denis.dechev@hivehome.com>
2022-11-04 22:00:58 +0200

    added tests and fixes for huffman

--- src/main/java/edu/onu/ddechev/codecs/Huffman.java
+++ src/main/java/edu/onu/ddechev/codecs/Huffman.java
@@ -1,44 +1,54 @@
 package edu.onu.ddechev.codecs;
 
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Huffman implements Codec {
+
+    Map<Byte, CodeValue> dict;
+
     @Override
     public void compress(SerializedImage serializedImage, ByteArrayOutputStream stream) throws IOException {
         byte[] data = serializedImage.data();
         Map<Byte, Integer> frequencyTable = getFrequencyTable(data);
         Node<TreeValue> node = buildTree(frequencyTable);
-        Map<Byte, DictValue> dict = new HashMap<>();
+        dict = new HashMap<>();
         buildDict(node, dict, new boolean[]{});
         compress(data, stream, dict);
     }
 
-    private void compress(byte[] data, ByteArrayOutputStream stream, Map<Byte, DictValue> dict) throws IOException {
-        writeTable(stream, dict);
-        ByteBuffer buffer = ByteBuffer.allocate(data.length);
+    void compress(byte[] data, ByteArrayOutputStream stream, Map<Byte, CodeValue> dict) throws IOException {
+        writeDict(stream, dict);
+        ByteBuffer buffer = ByteBuffer.allocate(data.length*3);
         int bitsWritten = 0;
         for (Byte b: data) {
             bitsWritten += dict.get(b).write(buffer, bitsWritten);
         }
-        stream.write(buffer.array());
+        int len = buffer.position();
+        if (bitsWritten % 8 != 0) {
+            len ++;
+        }
+        byte[] compressed = new byte[len];
+        buffer.position(0);
+        buffer.get(compressed, 0, compressed.length);
+        stream.write(compressed);
     }
 
-    private void writeTable(ByteArrayOutputStream streamNode, Map<Byte, DictValue> dict) {
-        for (Map.Entry<Byte, DictValue> entry: dict.entrySet()) {
-            streamNode.write(entry.getKey());
-            streamNode.write(entry.getValue().length);
-            streamNode.write(entry.getValue().value);
+    private void writeDict(ByteArrayOutputStream stream, Map<Byte, CodeValue> dict) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(stream);
+        dataOutputStream.writeByte(dict.size() - 1);
+        for (Map.Entry<Byte, CodeValue> entry: dict.entrySet()) {
+            dataOutputStream.writeByte(entry.getKey());
+            dataOutputStream.writeByte(entry.getValue().length);
+            dataOutputStream.writeShort(entry.getValue().value);
         }
     }
 
-    private Map<Byte, Integer> getFrequencyTable(byte[] data) {
+    static Map<Byte, Integer> getFrequencyTable(byte[] data) {
         ByteBuffer buffer = ByteBuffer.wrap(data);
         Map<Byte, Integer> result = new HashMap<>();
         while (buffer.hasRemaining()) {
@@ -52,13 +62,13 @@ public class Huffman implements Codec {
         return result;
     }
 
-    private void buildDict(Node<TreeValue> node, Map<Byte, DictValue> dict, boolean[] path) {
+    static void buildDict(Node<TreeValue> node, Map<Byte, CodeValue> dict, boolean[] path) {
         if ((node.left == null) && (node.right == null)) {
-            DictValue dictValue = new DictValue(path);
+            CodeValue codeValue = new CodeValue(path);
             if (node.value.values.length > 1) {
                 throw new IllegalStateException("Terminal node has more than one value");
             }
-            dict.put(node.value.values[0], dictValue);
+            dict.put(node.value.values[0], codeValue);
         } else {
             if (node.left != null) {
                 boolean[] nextPath = Arrays.copyOf(path, path.length + 1);
@@ -73,7 +83,7 @@ public class Huffman implements Codec {
         }
     }
 
-    private Node<TreeValue> buildTree(Map<Byte, Integer> frequencyTable) {
+    static Node<TreeValue> buildTree(Map<Byte, Integer> frequencyTable) {
         PriorityQueue<Node<TreeValue>> queue = frequencyTable.entrySet().stream()
                 .map(entry -> new Node<>(new TreeValue(entry.getValue(), new byte[]{entry.getKey()})))
                 .collect(Collectors.toCollection(PriorityQueue::new));
@@ -89,12 +99,60 @@ public class Huffman implements Codec {
 
     @Override
     public SerializedImage restore(ByteBuffer compressed, Integer width, Integer height) {
-        return null;
+        byte[] data = restore(compressed, readDict(compressed), width * height * 3);
+        return new SerializedImage(width, height, data);
+    }
+
+    byte[] restore(ByteBuffer compressed, Map<CodeValue, Byte> dict, int len) {
+        ByteBuffer restored = ByteBuffer.allocate(len);
+        List<Integer> codeLengths = dict.keySet().stream().map(c -> c.length).distinct().sorted().collect(Collectors.toList());
+        Map<Integer, List<Huffman.CodeValue>> codeByLength = dict.keySet().stream().collect(Collectors.groupingBy(c -> c.length));
+        int bitsRead = 0;
+        int count = 0;
+        while (count < len) {
+            for (int codeLength: codeLengths) {
+                int pos = compressed.position();
+                int value = CodeValue.read(compressed, bitsRead, codeLength);
+                Huffman.CodeValue code = null;
+                for (Huffman.CodeValue cv: codeByLength.get(codeLength)) {
+                    if (cv.value == value) {
+                        code = cv;
+                        break;
+                    }
+                }
+                if (code == null) {
+                    compressed.position(pos);
+                } else {
+                    restored.put(dict.get(code));
+                    bitsRead += codeLength;
+                    count++;
+                    break;
+                }
+            }
+        }
+        return restored.array();
+    }
+
+    static Map<CodeValue, Byte> readDict(ByteBuffer compressed) {
+        int dictLength = Byte.toUnsignedInt(compressed.get()) + 1;
+        Map<CodeValue, Byte> dict = new HashMap<>(dictLength);
+        for (int i = 0; i < dictLength; i++) {
+            byte b = compressed.get();
+            int codeLength = compressed.get();
+            int codeValue = compressed.getShort();
+            dict.put(new CodeValue(codeValue, codeLength), b);
+        }
+        return dict;
     }
 
     @Override
     public Map<String, Object> getLastCompressionProperties() {
-        return Map.of();
+        return Map.of(
+                "dict length", dict.size(),
+                "dict size, bytes", 4*dict.size()+1,
+                "shortest code", dict.values().stream().min(Comparator.comparingInt(cv -> cv.length)).map(cv -> cv.length).orElse(-1),
+                "longest code", dict.values().stream().max(Comparator.comparingInt(cv -> cv.length)).map(cv -> cv.length).orElse(-1)
+        );
     }
 
     static class TreeValue implements Comparable<TreeValue> {
@@ -120,6 +178,11 @@ public class Huffman implements Codec {
         public int compareTo(TreeValue treeValue) {
             return freq - treeValue.freq;
         }
+
+        @Override
+        public String toString() {
+            return String.format("[%s|%d]", new String(values) ,freq);
+        }
     }
 
     static class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
@@ -143,17 +206,38 @@ public class Huffman implements Codec {
         public int compareTo(Node<T> node) {
             return value.compareTo(node.value);
         }
+
+        @Override
+        public String toString() {
+            StringBuilder s = new StringBuilder("(");
+            s.append(value);
+            if (left != null) {
+                s.append("|l=");
+                s.append(left);
+            }
+            if (right != null) {
+                s.append("|r=");
+                s.append(right);
+            }
+            s.append(")");
+            return s.toString();
+        }
     }
 
-    static class DictValue {
+    static class CodeValue {
         private final int value;
-        private final byte length;
+        private final int length;
 
-        public DictValue(boolean[] treePath) {
+        public CodeValue(int value, int length) {
+            this.value = value;
+            this.length = length;
+        }
+
+        public CodeValue(boolean[] treePath) {
             this.length = Integer.valueOf(treePath.length).byteValue();
             int v = 0;
             for (int i = 0; i < length; i++) {
-                if (treePath[i]) {
+                if (treePath[length-i-1]) {
                     v += Math.pow(2, i);
                 }
             }
@@ -162,7 +246,7 @@ public class Huffman implements Codec {
 
         public int write(ByteBuffer buffer, int bitsWritten) {
             buffer.mark();
-            byte current = buffer.get();
+            int current = Byte.toUnsignedInt(buffer.get());
             buffer.reset();
             int bitsAvailable = 8 - bitsWritten % 8;
             int written = 0;
@@ -171,6 +255,7 @@ public class Huffman implements Codec {
                 newValue = current | (value >> (length - bitsAvailable));
                 written += bitsAvailable;
                 buffer.put(Integer.valueOf(newValue).byteValue());
+                buffer.mark();
             } else {
                 newValue = current | (value << (bitsAvailable - length));
                 written += length;
@@ -179,10 +264,11 @@ public class Huffman implements Codec {
             }
             while (written < length) {
                 int toWrite = length - written;
-                if (toWrite > 8) {
+                if (toWrite >= 8) {
                     newValue = 0xFF & (value >> (length - written - 8));
                     written += 8;
                     buffer.put(Integer.valueOf(newValue).byteValue());
+                    buffer.mark();
                 } else {
                     newValue = 0xFF & (value << (8 - toWrite));
                     written += toWrite;
@@ -191,6 +277,55 @@ public class Huffman implements Codec {
                 }
             }
             return written;
+        }
+
+        static int read(ByteBuffer buffer, int bitsRead, int codeLength) {
+            int bitsAvailable = 8 - bitsRead % 8;
+            buffer.mark();
+            int current = Byte.toUnsignedInt(buffer.get());
+            int read = 0;
+            if (bitsAvailable >= codeLength) {
+                if (bitsAvailable > codeLength) {
+                    buffer.reset();
+                }
+                return (current >> (bitsAvailable - codeLength)) & ((1 << codeLength) - 1);
+            } else {
+                int result = (current & ((1 << bitsAvailable) - 1)) << (codeLength - bitsAvailable);
+                read += bitsAvailable;
+                while (read < codeLength) {
+                    buffer.mark();
+                    current = Byte.toUnsignedInt(buffer.get());
+                    if (codeLength - read < 8) {
+                        result += (current >> (8 - codeLength + read)) & ((1 << (codeLength - read)) - 1);
+                        read += codeLength - read;
+                        buffer.reset();
+                    } else {
+                        result += current << (codeLength - read - 8);
+                        read += 8;
+                    }
+                }
+                return result;
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CodeValue codeValue = (CodeValue) o;
+            return value == codeValue.value && length == codeValue.length;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value, length);
+        }
+
+        @Override
+        public String toString() {
+            String code = Integer.toBinaryString(value);
+            String withLeadingZeros = String.format(String.format("%%%ds", length), code).replace(' ', '0');
+            return withLeadingZeros.substring(withLeadingZeros.length() - length);
         }
     }
 
---
+++ src/test/java/edu/onu/ddechev/codecs/HuffmanTest.java
@@ -0,0 +1,141 @@
+package edu.onu.ddechev.codecs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import static edu.onu.ddechev.codecs.Huffman.*;
+
+public class HuffmanTest {
+
+    @Test
+    public void endToEndTest1() throws IOException {
+        byte[] data = "ABCDEABCDEABCDEABCDEABCDEACDEACDEACDEADEADEDDD".getBytes(StandardCharsets.UTF_8);
+        Huffman huffman = new Huffman();
+        Map<Byte, Huffman.CodeValue> dict = new HashMap<>();
+        Map<Byte, Integer> table = getFrequencyTable(data);
+        System.out.println(table.entrySet().stream()
+                .map(e -> String.format("%s: %d", new String(new byte[]{e.getKey()}), e.getValue()))
+                .collect(Collectors.joining("\n")));
+        Node<TreeValue> tree = buildTree(table);
+        System.out.println(tree);
+        buildDict(tree, dict, new boolean[]{});
+        System.out.println(dict.entrySet().stream()
+                .map(e -> String.format("%s: %s", new String(new byte[]{e.getKey()}), e.getValue()))
+                .collect(Collectors.joining("\n")));
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        huffman.compress(data, stream, dict);
+
+        ByteBuffer buffer = ByteBuffer.wrap(stream.toByteArray());
+        Map<CodeValue, Byte> dictRestored = readDict(buffer);
+        System.out.println(dictRestored.entrySet().stream()
+                .map(e -> String.format("%s: %s", e.getKey(), new String(new byte[]{e.getValue()})))
+                .collect(Collectors.joining("\n")));
+        byte[] restored = huffman.restore(buffer, dictRestored, data.length);
+        System.out.println(new String(restored));
+        Assertions.assertArrayEquals(data, restored);
+    }
+
+    @Test
+    public void endToEndTest2() throws IOException {
+        byte[] data = new byte[]{-92, 116, 75, 67, -51, -45, 114, -27, 47, 113, 101, -26, -38, -43, -50, -101, -36, -12, -44, 6, 82, 14, 31, -23, -103, 123, -62, -36, -51, 44, 2, 99, -8, 88, -11, -103, 70, -107, -49, -117, -77, 42, 67, -99, 100, 10, 66, 60, 89, 25, -21, -38, -123, 91, -38, -112, 42, 24, -106, -127, -19, 38, 65, 42, 97, -112, -37, 69, -50, 122, 113, -46, -125, -79, 40, -74, 1, 4, 91, -45, 107, 79, 111, -100, -90, -105, 118, -4, 43, -104, 23, -128, 9, -51, -82, 41, -5, 78, -86, 16, 124, 31, -58, -115, -52, 110, 60, -27, 3, 60, -23, -128, 54, 110, 26, -52, 13, -59, 117, 68, -31, -89, -70, 74, -122, -122, 65, 90, -108, -11, 41, -1, 9, -33, 83, 91, -116, 41, 114, -118, 3, -105, -23, 100, -45, 16, 11, -89, -10, -116, -85, 99, -90, -38, -41, -98, 77, 22, 114, -98, 46, 116, -116, 118, 85, -58, 43, -40, 24, 109, 33, -12, 90, 9, -89, -105, -49, -96, 118, -58, 2, -72, 13, -13, 104, -7, -78, -34, 75, 7, 114, 31, -127, 124, 20, -40, -32, 19, 121, -84, -106, 63, -110, -82, -87, 75, -101, -78, -87, 48, -127, -81, -76, 54, -21, -60, 41, -95, -116, 111, -15, 40, 19, 77, 118, -124, -128, 54, 81, 72, 80, 60, -124, 40, -6, 10, 82, -97, 1, -100, 87, 38, 70, -125, -11, -71, -53, -49, 125, 49, -39, 51, -76, 27, 114, 31, -83, -104, -29, 11, -13, -58, 31, -16, 47, -121, -93, 29, -4, -94, 77, -45, -63, -75, 91, 54, -111, -37, 121, -33, 15, 11, -9, 98, -114, -19, -75, 73, -8, -2, -43, 65, 51, 6, 100, 49, 61, 57, 57, -16, 18, 55, -8, 80, 69, 43, 91, -106, -72, -42, -7, 16, 28, 30, -2, 16, 123, -58, 118, -27, -34, -88, 39, 113, 35, 80, -127, 106, 36, -111, 112, -118, 69, 114, 84, 40, 50, 6, 56, -21, 126, -48, 110, 52, -10, -56, -126, -78, -3, -92, -99, -66, 25, 118, 126, -7, -6, 18, -27, -110, -80, -100, -117, -64, 6, -90, 94, 126, 77, -29, 111, 49, 91, -80, 48, -50, -126, 6, 102, -55, -103, 42, -49, -49, 24, 71, -118, 116, -110, -63, -48, 18, -127, -69, 109, 108, 13, 115, -73, 20, 79, 55, 93, -63, -85, -120, -108, -106, -70, -20, 12, -79, 88, 120, -77, -81, 126, -33, -72, 19, -23, 67, 112, 20, -118, -123, 60, -45, -43, -19, 60, -100, 100, -39, 54, -73, 46, -113, 41, 6, 45, 123, -103, 49, 71, 27, -83, -89, -95, -104, -109, -86, -96, -9, 29, -86, -109, 1, -57, 126, 29, -45, -13, 62, -83, 93, -122, -106, -118, -91, 108, -78, 122, -71, -2, 26, -9, 8, -47, -20, 12, -28, 48, -125, 127, 64, -10, -117, -72, 43, 45, -65, 104, -52, 50, 85, -65, 63, 31, 115, -60, 113, 52, -3, -40, 37, 10, -86, -3, -85, 49, -4, -3, -99, 99, -99, -17, -88, 28, -111, -23, -53, -127, 94, 126, -60, 65, 6, 34, -35, -32, 96, -53, 6, 88, -33, -111, 115, -68, -30, 115, 0, -49, 107, -25, 47, -110, -92, 126, -110, 84, 115, 104, -100, 96, -51, -23, 75, -33, -88, -44, 63, -51, 10, -19, 28, 82, 101, -125, -103, -117, 92, 117, -64, -67, -103, -14, -126, 95, -69, 95, 118, -49, -35, 78, -64, -78, 52, 121, 13, 94, 20, -61, -72, 62, 25, 96, -38, -30, -54, -98, 48, 104, 54, -91, -96, 87, -125, 118, -62, -113, 55, -25, -75, 82, 66, 5, -118, 27, 102, -63, 82, 76, -73, 86, 74, 70, 126, -69, 43, -14, 0, 104, 34, -96, 9, -88, -100, -122, -74, -124, -18, -46, -117, 17, -5, 22, 114, -49, -111, -57, -103, -105, -104, -41, 52, -27, -30, 124, -42, 122, -84, 52, -49, 124, -89, -8, -69, -89, 83, 125, -65, 93, 62, 95, -24, -5, 40, 3, -126, 62, 28, -104, 69, -105, 51, 94, 126, 60, 103, -47, -53, 125, 31, 85, -79, 6, -106, -103, 2, -49, 116, 110, -83, -8, 82, -123, -114, 103, -36, 36, 20, -70, -48, -28, 30, -95, 4, -31, -38, 29, -115, 29, -49, -119, 119, -92, 5, 73, 0, -92, 64, -47, 59, 21, 73, -128, 57, -88, 110, 59, 124, -120, -70, -1, -65, 77, -123, 87, 61, 71, 18, -70, -123, -117, -116, -59, 111, -110, 33, 25, 35, -114, 45, 99, -3, 32, 96, -44, 61, 67, 107, 1, -38, -46, -55, -101, -70, 87, 42, -96, 52, -90, -2, 63, 120, 22, 54, 63, -76, 112, 102, 97, -92, -98, 88, -68, -61, -43, 24, 6, -115, -25, -122, 104, 98, 62, -100, 116, -9, -52, -58, 40, 75, -1, 25, 70, -52, -81, 30, 101, -102, 66, 127, -97, 33, 33, 48, 28, -5, 115, -103, -35, -89, 91, 86, -63, -35, -35, -9, 65, 10, 28, 20, -35, -33, 37, 59, 96, 68, -45, 39, -116, 105, -101, -122, 99, 14, 46, -84, 66, 46, 57, 111, 83, -91, 70, 75, -125, -64, 70, 5, 12, 42, 47, 13, 115, 84, -65, -104, -109, -24, -109, 50, 109, 51, -51, -15, -44, -30, 31, 30, -12, -46, 11, 9, 22, 69, 17, -49, -57, 11, 5, 6, -74, -57, 117, -81, -31, 33, -113, 85, -41, -64, 107, 28, -93, 60, -22, -60, 42, -102, 15, -17, 123, 38, 36, 74, 11, -46, -26, -80, -52, -67, 122, -60, 115, 116, 26, 18, -30, -4, -128, 77, -104, 95, 88, -49, 46, -11, 90, -63, 53, -127, -14, 72, 36, 26, 68, -79, -57, -44, -1, 28, 116, -46, 12, -119, 74, 24, -97, -82, 118, 10, 35, -105, 55, 60, 14, -108, -14, -128, 10, -9, -43, 84, 51, -65, -64, 79, 80, -47, 20, -106, -96, 46, -59, -45, -65, 58, -106, 120, 89, 35, 94, 36, -126, 49, -119, -35, 76, 36, 57, -48, 107, 94, 127, -80, -32, 12, 36, 38, 92, -32, 115, 113, 117, 49, 106, -6, -91, 51, -120};
+        Huffman huffman = new Huffman();
+        Map<Byte, Huffman.CodeValue> dict = new HashMap<>();
+        Map<Byte, Integer> table = getFrequencyTable(data);
+//        System.out.println(table.entrySet().stream()
+//                .map(e -> String.format("%s: %d", new String(new byte[]{e.getKey()}), e.getValue()))
+//                .collect(Collectors.joining("\n")));
+        Node<TreeValue> tree = buildTree(table);
+        //System.out.println(tree);
+        buildDict(tree, dict, new boolean[]{});
+//        System.out.println(dict.entrySet().stream()
+//                .map(e -> String.format("%s: %s", new String(new byte[]{e.getKey()}), e.getValue()))
+//                .collect(Collectors.joining("\n")));
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        huffman.compress(data, stream, dict);
+
+        ByteBuffer buffer = ByteBuffer.wrap(stream.toByteArray());
+        Map<CodeValue, Byte> dictRestored = readDict(buffer);
+//        System.out.println(dictRestored.entrySet().stream()
+//                .map(e -> String.format("%s: %s", e.getKey(), new String(new byte[]{e.getValue()})))
+//                .collect(Collectors.joining("\n")));
+        byte[] restored = huffman.restore(buffer, dictRestored, data.length);
+        System.out.println(new String(restored));
+        Assertions.assertArrayEquals(data, restored);
+    }
+
+    @Test
+    public void endToEndTest() throws IOException {
+        Huffman huffman = new Huffman();
+        for (int length = 1000; length <= 10000; length++) {
+            byte[] data = new byte[length];
+            new Random().nextBytes(data);
+            System.out.println(length);
+            System.out.println(Arrays.toString(data));
+            Map<Byte, Huffman.CodeValue> dict = new HashMap<>();
+            Map<Byte, Integer> table = getFrequencyTable(data);
+            Node<TreeValue> tree = buildTree(table);
+            buildDict(tree, dict, new boolean[]{});
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            huffman.compress(data, stream, dict);
+            ByteBuffer buffer = ByteBuffer.wrap(stream.toByteArray());
+            Map<CodeValue, Byte> dictRestored = readDict(buffer);
+            byte[] restored = huffman.restore(buffer, dictRestored, data.length);
+            System.out.println(Arrays.toString(restored));
+            Assertions.assertArrayEquals(data, restored);
+        }
+    }
+
+    @Test
+    public void readTest1() {
+        ByteBuffer buffer = ByteBuffer.wrap(new byte[]{(byte) 0b10101010, (byte) 0b10101010, (byte) 0b10101010});
+        int value = Huffman.CodeValue.read(buffer, 7, 11);
+        Assertions.assertEquals(0b01010101010, value);
+    }
+
+    @Test
+    public void readWriteTest1() {
+        int length = 11;
+        int offset = 7;
+        int value = 1422;
+        System.out.printf("value %d length %d offset %d\n", value, length, offset);
+        Huffman.CodeValue code = new Huffman.CodeValue(value, length);
+        ByteBuffer buffer = ByteBuffer.allocate(4);
+
+        int written = code.write(buffer, offset);
+        Assertions.assertEquals(length, written);
+        Assertions.assertArrayEquals(new byte[]{1, 0b0110_0011, (byte)0b1000_0000, 0}, buffer.array());
+        buffer.reset();
+        int valueRestored = Huffman.CodeValue.read(buffer, offset, length);
+        Assertions.assertEquals(value, valueRestored);
+    }
+
+    @Test
+    public void readWriteTest() {
+        Random r = new Random();
+        for (int length = 1; length <= 31; length++) {
+            for (int offset = 0; offset < 8; offset++) {
+                int value = r.nextInt() & ((1 << length) - 1);
+                System.out.printf("value %d length %d offset %d\n", value, length, offset);
+                Huffman.CodeValue code = new Huffman.CodeValue(value, length);
+                ByteBuffer buffer = ByteBuffer.allocate(6);
+
+                int written = code.write(buffer, offset);
+                Assertions.assertEquals(length, written);
+
+                buffer.reset();
+                int valueRestored = Huffman.CodeValue.read(buffer, offset, length);
+                Assertions.assertEquals(value, valueRestored);
+            }
+        }
+    }
+}
